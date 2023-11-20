e54d2df2bdf70bdf931c4f02246cf596bdfbda57
Vitaliy <kv1993@me.com>
2023-11-20 17:57:09 +0300

    Added main branch

--- Program.java
+++ Program.java
@@ -1,6 +1,9 @@
 class Program {
     public static void main(String[] args) {
-        System.out.println(123);
+
+        String str = "My string";
+
+        System.out.println(str);
     }
     
 }
